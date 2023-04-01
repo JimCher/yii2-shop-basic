5c5db173b2132d25e0d6091743339460c3b20e53
JimCher <11dmcher04@gmail.com>
2023-04-02 00:36:19 +0300

    front template

---
+++ assets/LtAppAsset.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\assets;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+/**
+ * Main application asset bundle.
+ */
+class LtAppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $js = [
+        'js/html5shiv.js',
+        'js/respond.min.js',
+    ];
+
+    public $jsOptions = [
+        'condition'=>'lt IE 9',
+        'position'=>View::POS_HEAD
+    ];
+}
